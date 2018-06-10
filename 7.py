25d3ed10325a3bd5f6c1963c5f0ea0c6aa96bc06
Bhagavath Singh <bhagavath.singh@wipro.com>
2018-06-10 21:42:57 +0530

    Added 7

--- 7.py
+++ 7.py
@@ -1,25 +1,18 @@
-inp=int(raw_input("Enter :"))
-ip_mask=[]
-def init(ip_mask):
-    for i in range(0,32):
-        ip_mask.append(0)
-    
-init(ip_mask)
-if inp!=0:
-    for i in range(0,inp):
-        ip_mask[i]=1
+inp=int(raw_input("Enter Prefix:"))
+if inp==32:
+    ip_mask=[1]*32
+elif inp>32:
+    print "Prefix not valid"
+    exit()
 else:
-    pass
-
-print ip_mask,len(ip_mask)
-a="".join(str(x) for x in ip_mask[:7])
-b="".join(str(x) for x in ip_mask[8:16])
-c="".join(str(x) for x in ip_mask[17:24])
-d="".join(str(x) for x in ip_mask[23:32])
-
-print a,b,c,d
-
-
-
-
-
+    ip_mask=[0]*32
+    for i in range(0,31):
+        if i<inp:
+            ip_mask[i]=1
+        else:
+            ip_mask[i]=0
+a=int("".join(str(x) for x in ip_mask[0:8]),2)
+b=int("".join(str(x) for x in ip_mask[8:16]),2)
+c=int("".join(str(x) for x in ip_mask[16:24]),2)
+d=int("".join(str(x) for x in ip_mask[24:32]),2)
+print "NetMask is : %d.%d.%d.%d" %(a,b,c,d)
